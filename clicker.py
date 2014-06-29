87fac2b88714f987a6450a5ebc3d7d0e3ecf6468
Gurio <re.stage00101@gmail.com>
2014-06-30 01:09:09 +0300

    logging fixed

--- clicker.py
+++ clicker.py
@@ -3,6 +3,7 @@
 
 import urllib2
 import urllib
+import cookielib
 import json
 import sys
 
@@ -11,41 +12,57 @@ def get_html (data):
 	response = urllib2.urlopen(req)
 	return response.read().decode('cp1251').encode('utf8')
 
-excepts=open('./exceptions', 'w+')
-log=open('./log', 'w+')
+with open('./exceptions', 'a') as excepts, open('./log', 'a') as log:
+	cj = cookielib.MozillaCookieJar()
+	cj.load('./cookies.txt') 
 
-opener = urllib2.build_opener()
-opener.addheaders.append(('Cookie', 'PHPSESSID=0skelcnh9k4fkrqcagf8gert37; mobile=n; eda=re.stage00101%40gmail.com%3A5Kdtf5thaZG42; __utma=125156309.2112618792.1404036061.1404036061.1404046268.2; __utmb=125156309.1.10.1404046268; __utmc=125156309; __utmz=125156309.1404036061.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ym_visorc_543098=w'))
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+	opener.addheaders = [	
+							('Accept-Language', 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,be;q=0.2'),
+							('Connection', 'keep-alive'),
+							('Content-Length','15'),
+							('Content-Type', 'application/x-www-form-urlencoded'),
+							('Cookie', 'PHPSESSID=0skelcnh9k4fkrqcagf8gert37; mobile=n; eda=re.stage00101%40gmail.com%3A5Kdtf5thaZG42; __utma=125156309.2112618792.1404036061.1404053044.1404056101.5; __utmb=125156309.12.10.1404056101; __utmc=125156309; __utmz=125156309.1404036061.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ym_visorc_543098=w'),
+							('Host','www.eda.by'),
+							('Origin', 'http://www.eda.by'),
+							('Referer', 'http://www.eda.by/my/history/'),
+							('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13')]
+	urllib2.install_opener(opener)
 
-p_type = ["11","12","13","14"]
-finish_clicks = [498, 348, 248, 148]
+	p_type = ["11","12","13","14"]
+	finish_clicks = [498, 348, 248, 148]
 
-fetch_val = {'act' : 'getcrazy'} 
+	fetch_val = {'act' : 'getcrazy'} 
 
-post_val = [[('act', 'crazyg'), ('r', p_type[0])],
-			[('act', 'crazyg'), ('r', p_type[1])],
-			[('act', 'crazyg'), ('r', p_type[2])]] # act:crazyg r:11,12,13
+	post_val = [[('act', 'crazyg'), ('r', p_type[0])],
+				[('act', 'crazyg'), ('r', p_type[1])],
+				[('act', 'crazyg'), ('r', p_type[2])]] # act:crazyg r:11,12,13
 
-fetch_data = urllib.urlencode(fetch_val)
-post_data = [urllib.urlencode(x) for x in post_val]
+	fetch_data = urllib.urlencode(fetch_val)
+	post_data = [urllib.urlencode(x) for x in post_val]
 
-print fetch_data
-print post_data
-
-while True:
-	try:
-		html = get_html(fetch_data)
-		jdata = json.loads(html)
-		#print jdata
-		for x in xrange(len(p_type)):
-			#print int(jdata[p_type[x]]['tek']), finish_clicks[x] 
-			if int(jdata[p_type[x]]['tek']) >= finish_clicks[x]:
-				log.write(get_html(post_data[x]))
-				log.write(get_html(fetch_data))
-				log.write(get_html(post_data[x]))
-				break
-	except:
-		excepts.write("Except!!!")
-		excepts.write(html)
-	else:
-		pass
+	print fetch_data
+	print post_data
+	log.write('LOG\n')
+	log.write(get_html(post_data[0])+'\n')
+	log.write(get_html(fetch_data)+'\n')
+	log.flush()
+	while True:
+		try:
+			html = get_html(fetch_data)
+			jdata = json.loads(html)
+			#print jdata
+			for x in xrange(len(p_type)):
+				#print int(jdata[p_type[x]]['tek']), finish_clicks[x] 
+				if int(jdata[p_type[x]]['tek']) >= finish_clicks[x]:
+					log.write(get_html(post_data[x])+'\n')
+					log.write(get_html(fetch_data)+'\n')
+					log.write(get_html(post_data[x])+'\n')
+					log.flush()
+					break
+		except:
+			excepts.write("Except: ")
+			excepts.write(html+'\n')
+			excepts.flush()
+		else:
+			pass
