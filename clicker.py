7fa711e48d91f037c4380244ab1911489b536ce8
Gurio <re.stage00101@gmail.com>
2014-07-01 15:13:45 +0300

    multiple requests fixes

--- clicker.py
+++ clicker.py
@@ -12,7 +12,7 @@ def get_html (openers, data):
 	responses = [opener.open('http://www.eda.by/enter.php', data) for opener in openers]
 	#req = urllib2.Request('http://www.eda.by/enter.php', data)
 	#response = urllib2.urlopen(req)
-	return [str(datetime.datetime.utcnow()) + ' ' + response.read().decode('cp1251').encode('utf8') + '\n' for response in responses]
+	return str([str(datetime.datetime.utcnow()) + ' ' + response.read().decode('cp1251').encode('utf8') + '\n' for response in responses])
 	
 def make_opener (cookie):
 	opener = urllib2.build_opener()#urllib2.HTTPCookieProcessor(cj))
@@ -34,7 +34,7 @@ with open('./exceptions', 'a') as excepts, open('./log', 'a') as log:
 	openers = [make_opener(x) for x in [
 'PHPSESSID=fen1abk47dct0v6s4ol6c103e5; mobile=n; __utma=125156309.1557815832.1403775274.1403792281.1404116627.4; __utmc=125156309; __utmz=125156309.1403775274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ym_visorc_543098=w; __utmb=125156309.15.10.1404116627; eda=broadcast.field%40gmail.com%3A5KykUWhXUTk0Y',
 'mobile=n; __utma=125156309.1557815832.1403775274.1404203947.1404211604.10; __utmz=125156309.1403775274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); eda=re.stage00101%40gmail.com%3A5Kdtf5thaZG42; PHPSESSID=agunapj56kgm6a5lq79g2dn2u1; __utmc=125156309',
-'mobile=n; eda=lopatsina%40yandex.by%3A5K1ZBrEeF5n3s; PHPSESSID=b5a9m6ltv8m2fmoln6r230bjh6; _ym_visorc_543098=w; __utma=125156309.1676049902.1404062398.1404132383.1404158777.5; __utmb=125156309.2.9.1404158778305; __utmc=125156309; __utmz=125156309.1404062398.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)']#urllib2.HTTPCookieProcessor(cj))
+'mobile=n; eda=lopatsina%40yandex.by%3A5K1ZBrEeF5n3s; PHPSESSID=b5a9m6ltv8m2fmoln6r230bjh6; _ym_visorc_543098=w; __utma=125156309.1676049902.1404062398.1404132383.1404158777.5; __utmb=125156309.2.9.1404158778305; __utmc=125156309; __utmz=125156309.1404062398.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)']]#urllib2.HTTPCookieProcessor(cj))
 	
 	lag = 7
         p_type = ["11","12","13","14"]
