b48c520c123db6b023e5a3b6d93543c4136d4e51
Gurio <re.stage00101@gmail.com>
2014-07-01 15:05:13 +0300

    multiple requests

--- clicker.py
+++ clicker.py
@@ -8,29 +8,36 @@ import datetime
 import json
 import sys
 
-def get_html (data):
-	req = urllib2.Request('http://www.eda.by/enter.php', data)
-	response = urllib2.urlopen(req)
-	return str(datetime.datetime.utcnow()) + ' ' + response.read().decode('cp1251').encode('utf8') + '\n'
-
-with open('./exceptions', 'a') as excepts, open('./log', 'a') as log:
-	#cj = cookielib.MozillaCookieJar()
-	#cj.load('./cookies.txt') 
-
+def get_html (openers, data):
+	responses = [opener.open('http://www.eda.by/enter.php', data) for opener in openers]
+	#req = urllib2.Request('http://www.eda.by/enter.php', data)
+	#response = urllib2.urlopen(req)
+	return [str(datetime.datetime.utcnow()) + ' ' + response.read().decode('cp1251').encode('utf8') + '\n' for response in responses]
+	
+def make_opener (cookie):
 	opener = urllib2.build_opener()#urllib2.HTTPCookieProcessor(cj))
 	opener.addheaders = [	
 							('Accept-Language', 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,be;q=0.2'),
 							('Connection', 'keep-alive'),
 							('Content-Length','15'),
 							('Content-Type', 'application/x-www-form-urlencoded'),
-							('Cookie', 'PHPSESSID=fen1abk47dct0v6s4ol6c103e5; mobile=n; __utma=125156309.1557815832.1403775274.1403792281.1404116627.4; __utmc=125156309; __utmz=125156309.1403775274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ym_visorc_543098=w; __utmb=125156309.15.10.1404116627; eda=broadcast.field%40gmail.com%3A5KykUWhXUTk0Y'),
+							('Cookie', cookie),
 							('Host','www.eda.by'),
 							('Origin', 'http://www.eda.by'),
 							('Referer', 'http://www.eda.by/my/history/'),
 							('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13')]
-	urllib2.install_opener(opener)
+	return opener
+with open('./exceptions', 'a') as excepts, open('./log', 'a') as log:
+	#cj = cookielib.MozillaCookieJar()
+	#cj.load('./cookies.txt') 
 
-	lag = 5
+	openers = [make_opener(x) for x in [
+		  			    'PHPSESSID=fen1abk47dct0v6s4ol6c103e5; mobile=n; __utma=125156309.1557815832.1403775274.1403792281.1404116627.4; __utmc=125156309; __utmz=125156309.1403775274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ym_visorc_543098=w; __utmb=125156309.15.10.1404116627; eda=broadcast.field%40gmail.com%3A5KykUWhXUTk0Y',
+					    'mobile=n; __utma=125156309.1557815832.1403775274.1404203947.1404211604.10; __utmz=125156309.1403775274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); eda=re.stage00101%40gmail.com%3A5Kdtf5thaZG42; PHPSESSID=agunapj56kgm6a5lq79g2dn2u1; __utmc=125156309',
+					    'mobile=n; eda=lopatsina%40yandex.by%3A5K1ZBrEeF5n3s; PHPSESSID=b5a9m6ltv8m2fmoln6r230bjh6; _ym_visorc_543098=w; __utma=125156309.1676049902.1404062398.1404132383.1404158777.5; __utmb=125156309.2.9.1404158778305; __utmc=125156309; __utmz=125156309.1404062398.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
+					   ]#urllib2.HTTPCookieProcessor(cj))
+	
+	lag = 7
         p_type = ["11","12","13","14"]
         finish_clicks = [x-lag for x in [500, 350, 250, 150]]
 
